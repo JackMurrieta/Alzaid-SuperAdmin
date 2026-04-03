a1b4f4c7e0d626a7627294fb9eb9ed7f25bc5f63
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-04-03 11:42:50 -0700

    feat: loading.component
    componente de carga cuando se realiza una petición a la api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingService } from './core/services/loading.service';
+import { LoadingComponent } from '../app/shared/components/loading.component/loading.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('alzaid-superadmin');
+  isLoading = false;
+  // inyectamos el servicio de loading para mostrar el spinner global
+  constructor(private loadingService: LoadingService) {
+    this.loadingService.loading$.subscribe((state) => {
+      this.isLoading = state;
+    });
+  }
+
 }
--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -1,7 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { LoadingService } from '../core/services/loading.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('token');
+    const loadingSvc = inject(LoadingService);
+
+    //mostrar loading en cada peticion a la api
+    loadingSvc.show();
 
     if (token) {
         req = req.clone({
@@ -11,5 +18,10 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         });
     }
 
-    return next(req);
+    return next(req).pipe(
+        finalize(() => {
+            // ocultar loading siempre (success o error)
+            loadingSvc.hide();
+        })
+    );
 };
